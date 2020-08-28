dc0ec8c94b956c4080b6354a3ba381ae4b1dd1f6
Nathan Rice <nrice@truecoders.io>
2020-08-28 18:23:29 -0500

    Finished 8 Ball, ready for push

---
+++ Magic_8_Ball/MagicBall.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Magic_8_Ball
+{
+    public class MagicBall
+    {
+        public static void MagicBallShaker()
+        {
+            Random randomResponse = new Random();
+            bool conditional = true;
+            Console.WriteLine("Welcome to the Virtual Magic 8 Ball");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Type question here ");
+            Console.ReadLine();
+
+            int responseIndex = randomResponse.Next(0, 19);
+
+            string currentResponse = Responses.responses[responseIndex];
+            Console.WriteLine(currentResponse);
+            Thread.Sleep(500);
+            ReRun();
+
+        }
+
+        public static void ReRun()
+        {
+            Console.WriteLine("Would you like to ask another question? Y or N");
+            string response = Console.ReadLine().ToLower();
+            if (response == "y")
+            {
+                Thread.Sleep(1500);
+                Console.Clear();
+                MagicBallShaker();
+            }
+            else
+            {
+                Environment.Exit(1);
+            }
+        }
+    }
+}
---
+++ Magic_8_Ball/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Magic_8_Ball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MagicBall.MagicBallShaker();
+        }
+    }
+}
